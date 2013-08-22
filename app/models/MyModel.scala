315b9a819c926ce55b29e1f67820f32c45398f69
wshino <wshinohara@gmail.com>
2013-08-22 11:09:39 +0900

    add my model spec

---
+++ app/models/MyModel.scala
@@ -0,0 +1,21 @@
+package models
+
+import com.novus.salat.global._
+import com.novus.salat.annotations._
+import com.mongodb.casbah.Imports._
+//import org.scala_tools.time.Imports._
+import com.novus.salat.dao.{ SalatDAO, ModelCompanion }
+import org.joda.time.DateTime
+//import com.github.nscala_time.time.Imports._
+
+object MyModel extends ModelCompanion[MyModel, ObjectId] {
+  val collection = MongoConnection()("my_db")("my_model_coll")
+  val dao = new SalatDAO[MyModel, ObjectId](collection = collection) {}
+
+  def findOneByName(name: String): Option[MyModel] = dao.findOne(MongoDBObject("name" -> name))
+  def findByDate(date: DateTime) = dao.find(MongoDBObject("date" -> date)).toList
+}
+
+case class MyModel(@Key("_id") id: ObjectId,
+                   name: String,
+                   date: DateTime)
---
+++ test/models/MyModelSpec.scala
@@ -0,0 +1,65 @@
+package models
+
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+import org.bson.types.ObjectId
+import org.joda.time.DateTime
+//import play.api.libs.json.Format
+//import com.github.nscala_time.time.Imports._
+import com.mongodb.casbah.Imports._
+//import com.mongodb.casbah.Imports.ObjectId
+import play.api.Play.current
+import se.radley.plugin.salat._
+import se.radley.plugin.salat.Formats._
+import com.novus.salat._
+import com.mongodb.casbah.commons.conversions.scala._
+
+/**
+ * Add your spec here.
+ * You can mock out a whole application including requests, plugins etc.
+ * For more information, consult the wiki.
+ */
+class MyModelSpec extends Specification {
+  
+  "MyModel" should {
+
+    // jodatime のシリアライズの場合は必ず書くこと
+    RegisterJodaTimeConversionHelpers()
+    RegisterConversionHelpers()
+
+    "try" in {
+      running(FakeApplication()) {
+
+        // mymodel の中身を空にする
+        MyModel.remove(MongoDBObject.empty)
+
+        val now = new DateTime
+        val m = MyModel.apply(id = new ObjectId, name = "hoge", date = now)
+        println(s"m = $m")
+
+        val dbo = MyModel.toDBObject(m)
+        println(s"dbo = $dbo")
+        val cj = MyModel.toCompactJson(m)
+        println(s"cj = $cj")
+
+        val res = MyModel.insert(m)
+        println(s"res = $res")
+
+        MyModel.count(MongoDBObject("name" -> "hoge")) mustEqual 1
+
+        MyModel.findByDate(now).head.name mustEqual "hoge"
+
+        MyModel.findOneByName("hoge").map( r => r.name mustEqual "hoge")
+
+        MyModel.findOneByName("fuga") match {
+          case Some(x) => false mustEqual true
+          case _ => true mustEqual true
+        }
+
+      }
+    }
+    
+  }
+}
